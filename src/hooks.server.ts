1499772c0249da2fa68fddeddb29f8962c0ee6a9
Yoni Rapoport <yoni@fireflymigration.com>
2023-11-19 14:19:33 +0200

    test1

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,14 @@
-// import { sequence } from '@sveltejs/kit/hooks'
-// import { handleRemult } from './hooks/handleRemult'
+import { sequence } from '@sveltejs/kit/hooks'
+import { handleRemult } from './hooks/handleRemult'
 
-import type { Handle } from "@sveltejs/kit";
+// import type { Handle } from "@sveltejs/kit";
 
-// export const handle = sequence(handleRemult)
+export const handle = sequence(handleRemult)
 
-export const handle: Handle = async ({ event, resolve }) => {
+// export const handle: Handle = async ({ event, resolve }) => {
 
-    if (event.url.pathname.startsWith("/api"))
-        return new Response('hello world');
+//     if (event.url.pathname.startsWith("/api"))
+//         return new Response('hello world');
 
-    return await resolve(event);
-}
+//     return await resolve(event);
+// }
